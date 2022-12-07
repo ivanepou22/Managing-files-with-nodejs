736331413922d1d6194a0b0ea8dd1b658499f4bd
Ivan Epou <ivanepou92@gmail.com>
2022-12-08 00:11:06 +0300

    Read a chunck of data from a file

--- asynchronous.read.chunk.js
+++ asynchronous.read.chunk.js
@@ -1,2 +1,17 @@
 const fs = require('fs');
 
+//determine the size of the file
+let totalSize = 0;
+fs.stat('./data/app.log', (err, {size}) => totalSize = size);
+
+fs.open(
+    './data/app.log', (err, fd) => {
+        const buffer = Buffer.alloc(200);
+
+        //read the file
+        for(let i = 0; i <= totalSize/buffer.length; i++) {
+            fs.read(fd, buffer, 0, buffer.length, i*buffer.length, (err, count, buff) => {
+                console.log(buff.toString());
+            })
+        }
+    })
