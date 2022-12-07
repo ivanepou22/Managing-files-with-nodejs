ebf86853e0db565ea8b8d33cc2264a443859b13e
Ivan Epou <ivanepou92@gmail.com>
2022-12-07 23:41:18 +0300

    Reading partial data from a file

--- asynchronous.read.partial.js
+++ asynchronous.read.partial.js
@@ -5,8 +5,10 @@ open("./data/pulitzer-circulation-data.csv", (err, fd) => {
     //create anew buffer and allocate it to be 200bytes long
     const buffer = Buffer.alloc(200);
 
-    //read to the buffer
+    //read to the buffer read((file descriptor)fd, buffer where the data is read to, where to start storing data in the buffer, amount of bytes to read, where to start reading, callback function )
     read(fd, buffer, 0, buffer.length, 0, (err, count, buff) => {
+        //count = Amt of data that was read
+        //buff = Data that was read
         console.table(convertCsv(buff.toString()));
     });
 })
